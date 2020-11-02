00507c3efe2773366e8e2ffbe44d57b86536e7f8
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-11-02 15:13:15 -0300

    Add manipulation of the view that has selection of user

--- app/src/main/java/br/com/touchsoul/aguaegas/View/SelectUser.java
+++ app/src/main/java/br/com/touchsoul/aguaegas/View/SelectUser.java
@@ -1,5 +1,6 @@
 package br.com.touchsoul.aguaegas.View;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -33,7 +34,8 @@ public class SelectUser extends AppCompatActivity {
         userBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
+                //finish();
+                startActivity(new Intent(getApplicationContext(), ChooseService.class));
             }
         });
     }
