583b42af4f3ceacc91a1c1c9a4f1f7d91a4ea4f1
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-11-07 15:28:37 -0300

    Add water search activity

--- app/src/main/java/br/com/touchsoul/aguaegas/View/Login.java
+++ app/src/main/java/br/com/touchsoul/aguaegas/View/Login.java
@@ -1,7 +1,7 @@
 package br.com.touchsoul.aguaegas.View;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
---
+++ app/src/main/java/br/com/touchsoul/aguaegas/View/MapWaterCustomer.java
@@ -0,0 +1,17 @@
+package br.com.touchsoul.aguaegas.View;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import br.com.touchsoul.aguaegas.R;
+
+public class MapWaterCustomer extends AppCompatActivity {
+    @Override
+    protected  void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_water_customer);
+
+
+    }
+}
