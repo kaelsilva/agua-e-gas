7186146c3688587ffa8a96af52915610808c7d0a
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-11-27 17:29:50 -0300

    Fix logout function

--- app/src/main/java/br/com/touchsoul/aguaegas/View/ProviderMenu.java
+++ app/src/main/java/br/com/touchsoul/aguaegas/View/ProviderMenu.java
@@ -5,21 +5,48 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import br.com.touchsoul.aguaegas.R;
 
 public class ProviderMenu extends AppCompatActivity {
     private ImageView btnLogout, btnCloseapp;
     private Button btnConfigurations, btnSeeOrders;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.provider_menu);
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
         buttonConfig();
+    }
+
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(ProviderMenu.this, "Deslogado com sucesso!",
+                                Toast.LENGTH_LONG).show();
+                        finish();
+                    }
+                });
     }
 
     public void buttonConfig(){
@@ -31,7 +58,13 @@ public class ProviderMenu extends AppCompatActivity {
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
+                switch (view.getId()) {
+                    // ...
+                    case R.id.btn_logout:
+                        signOut();
+                        break;
+                    // ...
+                }
             }
         });
 
