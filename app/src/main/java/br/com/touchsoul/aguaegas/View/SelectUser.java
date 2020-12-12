0fed799d271b2e60a92ffbb7e095eec59ccb945f
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-12-11 22:04:48 -0300

    Add user registration to database

--- app/src/main/java/br/com/touchsoul/aguaegas/View/SelectUser.java
+++ app/src/main/java/br/com/touchsoul/aguaegas/View/SelectUser.java
@@ -10,6 +10,14 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -17,11 +25,17 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
 import br.com.touchsoul.aguaegas.R;
 
 public class SelectUser extends AppCompatActivity {
     private ImageView backBtn, userBtn, providerBtn;
     private GoogleSignInClient mGoogleSignInClient;
+    GoogleSignInAccount acct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +50,7 @@ public class SelectUser extends AppCompatActivity {
 
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
-        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        acct = GoogleSignIn.getLastSignedInAccount(this);
         if (acct != null) {
             String personName = acct.getDisplayName();
             String personEmail = acct.getEmail();
@@ -78,6 +92,21 @@ public class SelectUser extends AppCompatActivity {
         userBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                try {
+                    Toast.makeText(SelectUser.this, "Vai fazer o POST",
+                            Toast.LENGTH_SHORT).show();
+                    register(new JSONObject("{\"googleid\": \""+acct.getId()+"\"," +
+                                            "\"name\": \""+acct.getDisplayName()+"\"," +
+                                            "\"email\": \""+acct.getEmail()+"\"," +
+                                            "\"usertype\": \"1\"," +
+                                            "\"latitude\": \"0\"," +
+                                            "\"longitude\": \"0\"" +
+                                            "}"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(SelectUser.this, "Erro no POST: "+e.toString(),
+                            Toast.LENGTH_LONG).show();
+                }
                 finish();
                 startActivity(new Intent(getApplicationContext(), ChooseService.class));
             }
@@ -90,5 +119,40 @@ public class SelectUser extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), ProviderMenu.class));
             }
         });
+    }
+
+    public void register(final JSONObject jsonObj) throws JSONException {
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://192.168.0.129:3000/users";
+
+        // Post params to be sent to the server
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("googleid", jsonObj.getString("googleid"));
+        params.put("name", jsonObj.getString("name"));
+        params.put("email", jsonObj.getString("email"));
+        params.put("usertype", jsonObj.getString("usertype"));
+
+        // Request a string response from the provided URL.
+        JsonObjectRequest jsonRequest = new JsonObjectRequest(url, new JSONObject(params),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // your response
+                        Toast.makeText(SelectUser.this, "Usuário cadastrado com sucesso.",
+                                Toast.LENGTH_SHORT).show();
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // error
+                //Toast.makeText(SelectUser.this, "Erro em register(): "+error.toString(),
+                //        Toast.LENGTH_LONG).show();
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(jsonRequest);
+        //requestQueue.start();
     }
 }
